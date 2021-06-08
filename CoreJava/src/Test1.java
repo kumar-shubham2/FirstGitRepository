a23e486423527779653177fc1834c5bd387c6831
Shubham_Kumar_Sharma <shubhamsharmak1998@gmail.com>
2021-06-08 16:59:03 +0530

    Added Test1.java

---
+++ CoreJava/src/Test1.java
@@ -0,0 +1,10 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the new world of GIT...\n "
+				+ "Now you can push your code from here....");
+	}
+
+}
